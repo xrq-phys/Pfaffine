50759d4cf136c805d30693b3b79dc8f7c0bd6041
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-05-12 05:06:25 +0900

    Added SVE Kernels for Complex
    TODO: Do adaption.

---
+++ src/kernel/zmgemm_sve2wx14.s
@@ -0,0 +1,451 @@
+// For computing complex micro gemm for W<M<2W, N<14 and K,
+//   where W is {SVE vector length in words}/4.
+// Parameger registers are pointers:
+// x0: =>[M,N,K]
+// x1: =>[ALPHA,BETA]
+// x2: =>A
+// x3: LDA
+// x4: =>B
+// x5: LDB
+// x6: =>C
+// x7: LDC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+	.arch	armv8.2-a+sve
+	.text
+	.global	zmgemm_2wx14
+	.type	zmgemm_2wx14, %function
+zmgemm_2wx14:
+	.cfi_startproc
+	ldp	x9, x10, [x0], #16	// shape M and N.
+	mov	x15, #2
+	mul	x14, x9, x15	// shape M in doubles.
+	ldr	x8, [x0]	// loop parameter K.
+	mov	x11, xzr
+	mov	x12, #16	// 4-byte complex size
+	incd	x11	// determine vector length, in doubles.
+	ptrue	p0.d, all	// first half is all-true.
+	whilelo	p1.d, x11, x14	// second half from M argument.
+	fmov	d0, #1.0	// exact float 1.0, see below.
+	fmov	x14, d0	// use hard float for strict comparison.
+// Register configuration:
+// Z[30-31]: A columns
+// Z29: not used
+// Z0: B elements broadcasted
+// Z[1-28]: C change buffer
+	fmov	z1.d, p0/m, #0.0
+	fmov	z2.d, p0/m, #0.0
+	fmov	z3.d, p0/m, #0.0
+	fmov	z4.d, p0/m, #0.0
+	fmov	z5.d, p0/m, #0.0
+	fmov	z6.d, p0/m, #0.0
+	fmov	z7.d, p0/m, #0.0
+	fmov	z8.d, p0/m, #0.0
+	fmov	z9.d, p0/m, #0.0
+	fmov	z10.d, p0/m, #0.0
+	fmov	z11.d, p0/m, #0.0
+	fmov	z12.d, p0/m, #0.0
+	fmov	z13.d, p0/m, #0.0
+	fmov	z14.d, p0/m, #0.0
+	fmov	z15.d, p0/m, #0.0
+	fmov	z16.d, p0/m, #0.0
+	fmov	z17.d, p0/m, #0.0
+	fmov	z18.d, p0/m, #0.0
+	fmov	z19.d, p0/m, #0.0
+	fmov	z20.d, p0/m, #0.0
+	fmov	z21.d, p0/m, #0.0
+	fmov	z22.d, p0/m, #0.0
+	fmov	z23.d, p0/m, #0.0
+	fmov	z24.d, p0/m, #0.0
+	fmov	z25.d, p0/m, #0.0
+	fmov	z26.d, p0/m, #0.0
+	fmov	z27.d, p0/m, #0.0
+	fmov	z28.d, p0/m, #0.0
+K_LOOP:
+// Load columns from A.
+	ld1d	z30.d, p0/z, [x2]
+	ld1d	z31.d, p1/z, [x2, x11, lsl 3]	// second vector
+	madd	x2, x3, x12, x2	// move forward
+// Apply B columns.
+	mov	x13, x10	// counter
+	ld1rqd	z0.d, p0/z, [x4, #0]	// row L column 0
+	fcmla	z1.d, p0/m, z30.d, z0.d, #0
+	fcmla	z1.d, p0/m, z30.d, z0.d, #90
+	fcmla	z2.d, p1/m, z31.d, z0.d, #0
+	fcmla	z2.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #16]	// row L column 1
+	fcmla	z3.d, p0/m, z30.d, z0.d, #0
+	fcmla	z3.d, p0/m, z30.d, z0.d, #90
+	fcmla	z4.d, p1/m, z31.d, z0.d, #0
+	fcmla	z4.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #32]	// row L column 2
+	fcmla	z5.d, p0/m, z30.d, z0.d, #0
+	fcmla	z5.d, p0/m, z30.d, z0.d, #90
+	fcmla	z6.d, p1/m, z31.d, z0.d, #0
+	fcmla	z6.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #48]	// row L column 3
+	fcmla	z7.d, p0/m, z30.d, z0.d, #0
+	fcmla	z7.d, p0/m, z30.d, z0.d, #90
+	fcmla	z8.d, p1/m, z31.d, z0.d, #0
+	fcmla	z8.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #64]	// row L column 4
+	fcmla	z9.d, p0/m, z30.d, z0.d, #0
+	fcmla	z9.d, p0/m, z30.d, z0.d, #90
+	fcmla	z10.d, p1/m, z31.d, z0.d, #0
+	fcmla	z10.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #80]	// row L column 5
+	fcmla	z11.d, p0/m, z30.d, z0.d, #0
+	fcmla	z11.d, p0/m, z30.d, z0.d, #90
+	fcmla	z12.d, p1/m, z31.d, z0.d, #0
+	fcmla	z12.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #96]	// row L column 6
+	fcmla	z13.d, p0/m, z30.d, z0.d, #0
+	fcmla	z13.d, p0/m, z30.d, z0.d, #90
+	fcmla	z14.d, p1/m, z31.d, z0.d, #0
+	fcmla	z14.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x4, #112]	// row L column 7
+	fcmla	z15.d, p0/m, z30.d, z0.d, #0
+	fcmla	z15.d, p0/m, z30.d, z0.d, #90
+	fcmla	z16.d, p1/m, z31.d, z0.d, #0
+	fcmla	z16.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+// Update base address due to #imm limitations.
+	add	x16, x4, #128
+	ld1rqd	z0.d, p0/z, [x16, #0]	// row L column 8
+	fcmla	z17.d, p0/m, z30.d, z0.d, #0
+	fcmla	z17.d, p0/m, z30.d, z0.d, #90
+	fcmla	z18.d, p1/m, z31.d, z0.d, #0
+	fcmla	z18.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x16, #16]	// row L column 9
+	fcmla	z19.d, p0/m, z30.d, z0.d, #0
+	fcmla	z19.d, p0/m, z30.d, z0.d, #90
+	fcmla	z20.d, p1/m, z31.d, z0.d, #0
+	fcmla	z20.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x16, #32]	// row L column 10
+	fcmla	z21.d, p0/m, z30.d, z0.d, #0
+	fcmla	z21.d, p0/m, z30.d, z0.d, #90
+	fcmla	z22.d, p1/m, z31.d, z0.d, #0
+	fcmla	z22.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x16, #48]	// row L column 11
+	fcmla	z23.d, p0/m, z30.d, z0.d, #0
+	fcmla	z23.d, p0/m, z30.d, z0.d, #90
+	fcmla	z24.d, p1/m, z31.d, z0.d, #0
+	fcmla	z24.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x16, #64]	// row L column 12
+	fcmla	z25.d, p0/m, z30.d, z0.d, #0
+	fcmla	z25.d, p0/m, z30.d, z0.d, #90
+	fcmla	z26.d, p1/m, z31.d, z0.d, #0
+	fcmla	z26.d, p1/m, z31.d, z0.d, #90
+	subs	x13, x13, #1
+	b.eq	NEXT_ROW
+	ld1rqd	z0.d, p0/z, [x16, #80]	// row L column 13
+	fcmla	z27.d, p0/m, z30.d, z0.d, #0
+	fcmla	z27.d, p0/m, z30.d, z0.d, #90
+	fcmla	z28.d, p1/m, z31.d, z0.d, #0
+	fcmla	z28.d, p1/m, z31.d, z0.d, #90
+//	subs	x13, x13, #1
+//	b.eq	NEXT_ROW
+NEXT_ROW:
+	madd	x4, x5, x12, x4	// move forward
+	subs	x8, x8, #1
+	b.ne	K_LOOP	// next column / row.
+WRITE_MEM:
+// Override A and B buffers:
+// z[30-31]: real part and complete entries of alpha.
+// z0: C memory buffer.
+// z29: zeroized.
+	ldp	x16, x17, [x1]	// alpha, as 128-bits
+	ld1rqd	z30.d, p0/z, [x1]	// alpha, to the vector.
+	ld1rd	z31.d, p0/z, [x1]	// alpha, real parts to the vector.
+// (R&)Write data back to C memory.
+	cmp	x14, x16
+	b.ne	NONUNIT_ALPHA
+	cmp	xzr, x17
+	b.eq	UNIT_ALPHA
+NONUNIT_ALPHA:
+// Honestly speaking this is not optimal here,
+// but skr2k is called only with alpha=1.0 in Pfaffine.
+// TODO: Separately handle beta real and beta complex.
+	fmov	z29.d, p0/m, #0.0	// 0-memory buffer.
+	fadd	z0.d, z1.d, z29.d
+	fmul	z1.d, z1.d, z31.d	// magnify z1: real part
+	fcmla	z1.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z2.d, z29.d
+	fmul	z2.d, z2.d, z31.d	// magnify z2: real part
+	fcmla	z2.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z3.d, z29.d
+	fmul	z3.d, z3.d, z31.d	// magnify z3: real part
+	fcmla	z3.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z4.d, z29.d
+	fmul	z4.d, z4.d, z31.d	// magnify z4: real part
+	fcmla	z4.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z5.d, z29.d
+	fmul	z5.d, z5.d, z31.d	// magnify z5: real part
+	fcmla	z5.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z6.d, z29.d
+	fmul	z6.d, z6.d, z31.d	// magnify z6: real part
+	fcmla	z6.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z7.d, z29.d
+	fmul	z7.d, z7.d, z31.d	// magnify z7: real part
+	fcmla	z7.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z8.d, z29.d
+	fmul	z8.d, z8.d, z31.d	// magnify z8: real part
+	fcmla	z8.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z9.d, z29.d
+	fmul	z9.d, z9.d, z31.d	// magnify z9: real part
+	fcmla	z9.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z10.d, z29.d
+	fmul	z10.d, z10.d, z31.d	// magnify z10: real part
+	fcmla	z10.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z11.d, z29.d
+	fmul	z11.d, z11.d, z31.d	// magnify z11: real part
+	fcmla	z11.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z12.d, z29.d
+	fmul	z12.d, z12.d, z31.d	// magnify z12: real part
+	fcmla	z12.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z13.d, z29.d
+	fmul	z13.d, z13.d, z31.d	// magnify z13: real part
+	fcmla	z13.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z14.d, z29.d
+	fmul	z14.d, z14.d, z31.d	// magnify z14: real part
+	fcmla	z14.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z15.d, z29.d
+	fmul	z15.d, z15.d, z31.d	// magnify z15: real part
+	fcmla	z15.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z16.d, z29.d
+	fmul	z16.d, z16.d, z31.d	// magnify z16: real part
+	fcmla	z16.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z17.d, z29.d
+	fmul	z17.d, z17.d, z31.d	// magnify z17: real part
+	fcmla	z17.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z18.d, z29.d
+	fmul	z18.d, z18.d, z31.d	// magnify z18: real part
+	fcmla	z18.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z19.d, z29.d
+	fmul	z19.d, z19.d, z31.d	// magnify z19: real part
+	fcmla	z19.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z20.d, z29.d
+	fmul	z20.d, z20.d, z31.d	// magnify z20: real part
+	fcmla	z20.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z21.d, z29.d
+	fmul	z21.d, z21.d, z31.d	// magnify z21: real part
+	fcmla	z21.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z22.d, z29.d
+	fmul	z22.d, z22.d, z31.d	// magnify z22: real part
+	fcmla	z22.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z23.d, z29.d
+	fmul	z23.d, z23.d, z31.d	// magnify z23: real part
+	fcmla	z23.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z24.d, z29.d
+	fmul	z24.d, z24.d, z31.d	// magnify z24: real part
+	fcmla	z24.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z25.d, z29.d
+	fmul	z25.d, z25.d, z31.d	// magnify z25: real part
+	fcmla	z25.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z26.d, z29.d
+	fmul	z26.d, z26.d, z31.d	// magnify z26: real part
+	fcmla	z26.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z27.d, z29.d
+	fmul	z27.d, z27.d, z31.d	// magnify z27: real part
+	fcmla	z27.d, p0/m, z30.d, z0.d, #90	// imaginary part
+	fadd	z0.d, z28.d, z29.d
+	fmul	z28.d, z28.d, z31.d	// magnify z28: real part
+	fcmla	z28.d, p0/m, z30.d, z0.d, #90	// imaginary part
+// Unit alpha case.
+UNIT_ALPHA:
+// Non-pure-real beta here yields one more instruction.
+// TODO: separating real-alpha-beta situations might be necessary.
+// Override alpha buffers with beta.
+// Note: now z31.d is complete and z30.d is real part vector.
+	ld1rqd	z31.d, p0/z, [x1, #16]	// full entries of beta.
+	ld1rd	z30.d, p0/z, [x1, #16]	// real part of beta.
+//	mov	x10, x10	// x10 itself acts as counter.
+	ld1d	z0.d, p0/z, [x6]	// column vector 0
+	fcmla	z1.d, p0/m, z31.d, z0.d, #90	// im(beta)I * C +-> delta(C)
+	fmad	z0.d, p0/m, z30.d, z1.d	// delta(C) +-> re(beta) * C
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z2.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z2.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 1
+	fcmla	z3.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z3.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z4.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z4.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 2
+	fcmla	z5.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z5.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z6.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z6.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 3
+	fcmla	z7.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z7.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z8.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z8.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 4
+	fcmla	z9.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z9.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z10.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z10.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 5
+	fcmla	z11.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z11.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z12.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z12.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 6
+	fcmla	z13.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z13.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z14.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z14.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 7
+	fcmla	z15.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z15.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z16.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z16.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 8
+	fcmla	z17.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z17.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z18.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z18.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 9
+	fcmla	z19.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z19.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z20.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z20.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 10
+	fcmla	z21.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z21.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z22.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z22.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 11
+	fcmla	z23.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z23.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z24.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z24.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 12
+	fcmla	z25.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z25.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z26.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z26.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+	subs	x10, x10, #1
+	madd	x6, x7, x12, x6
+	b.eq	END_WRITE_MEM
+	ld1d	z0.d, p0/z, [x6]	// column vector 13
+	fcmla	z27.d, p0/m, z31.d, z0.d, #90
+	fmad	z0.d, p0/m, z30.d, z27.d
+	st1d	z0.d, p0, [x6]
+	ld1d	z0.d, p1/z, [x6, x11, lsl #3]
+	fcmla	z28.d, p1/m, z31.d, z0.d, #90
+	fmad	z0.d, p1/m, z30.d, z28.d
+	st1d	z0.d, p1, [x6, x11, lsl #3]
+//	subs	x10, x10, #1
+//	madd	x6, x7, x12, x6
+//	b.eq	END_WRITE_MEM
+// End of computation.
+END_WRITE_MEM:
+	mov	x0, #0	// return normal.
+	b	END_EXEC
+END_ERROR:
+	mov	x0, #1	// return error.
+END_EXEC:
+	ret
+	.cfi_endproc
