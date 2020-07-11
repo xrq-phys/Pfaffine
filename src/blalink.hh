47d91f3d7303a342bf8dcf4c770a9c89e9712fd7
Ruqing Xu <r-xu@ai-c01.phys.s.u-tokyo.ac.jp>
2020-07-11 19:03:01 +0900

    Blalink Links to BLIS

--- src/blalink.hh
+++ src/blalink.hh
@@ -1,6 +1,6 @@
 /**
  * \file blalink.hh
- * C++ to C type wrapper for BLAS.
+ * C++ to C type wrapper for BLIS type-apis.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,139 +8,126 @@
  */
 #pragma once
 #include <complex>
-typedef std::complex<float>  scomplex;
-typedef std::complex<double> dcomplex;
-typedef struct {
-    float real;
-    float imag;
-} cscomplex;
-typedef struct {
-    double real;
-    double imag;
-} cdcomplex;
+typedef std::complex<float>  ccscmplx;
+typedef std::complex<double> ccdcmplx;
+// BLIS definitions.
+#include "blis.h"
 
 
 // gemm
-extern "C" {
-void sgemm_(char *transa, char *transb, unsigned *m, unsigned *n, unsigned *k, float *alpha, 
-            float *a, unsigned *lda, float *b, unsigned *ldb, float *beta, float *c, unsigned *ldc);
-void dgemm_(char *transa, char *transb, unsigned *m, unsigned *n, unsigned *k, double *alpha, 
-            double *a, unsigned *lda, double *b, unsigned *ldb, double *beta, double *c, unsigned *ldc);
-void cgemm_(char *transa, char *transb, unsigned *m, unsigned *n, unsigned *k, void *alpha, 
-            void *a, unsigned *lda, void *b, unsigned *ldb, void *beta, void *c, unsigned *ldc);
-void zgemm_(char *transa, char *transb, unsigned *m, unsigned *n, unsigned *k, void *alpha, 
-            void *a, unsigned *lda, void *b, unsigned *ldb, void *beta, void *c, unsigned *ldc);
-}
-inline void gemm(char transa, char transb, unsigned m, unsigned n, unsigned k, float alpha,
-                 float *a, unsigned lda, float *b, unsigned ldb, float beta, float *c, unsigned ldc)
-{ sgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc); }
-inline void gemm(char transa, char transb, unsigned m, unsigned n, unsigned k, double alpha,
-                 double *a, unsigned lda, double *b, unsigned ldb, double beta, double *c, unsigned ldc)
-{ dgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc); }
-inline void gemm(char transa, char transb, unsigned m, unsigned n, unsigned k, scomplex alpha,
-                 scomplex *a, unsigned lda, scomplex *b, unsigned ldb, scomplex beta, scomplex *c, unsigned ldc)
-{ cgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc); }
-inline void gemm(char transa, char transb, unsigned m, unsigned n, unsigned k, dcomplex alpha,
-                 dcomplex *a, unsigned lda, dcomplex *b, unsigned ldb, dcomplex beta, dcomplex *c, unsigned ldc)
-{ zgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc); }
+template <typename T>
+inline void gemm(trans_t transa, trans_t transb,
+                 dim_t m, dim_t n, dim_t k,
+                 T alpha,
+                 T *a, inc_t lda,
+                 T *b, inc_t ldb,
+                 T beta,
+                 T *c, inc_t ldc);
+template <> inline void gemm<float>(trans_t transa, trans_t transb, dim_t m, dim_t n, dim_t k, float alpha, float *a, inc_t lda, float *b, inc_t ldb, float beta, float *c, inc_t ldc)
+{ bli_sgemm(transa, transb, m, n, k, &alpha, a, 1, lda, b, 1, ldb, &beta, c, 1, ldc); }
+template <> inline void gemm<double>(trans_t transa, trans_t transb, dim_t m, dim_t n, dim_t k, double alpha, double *a, inc_t lda, double *b, inc_t ldb, double beta, double *c, inc_t ldc)
+{ bli_dgemm(transa, transb, m, n, k, &alpha, a, 1, lda, b, 1, ldb, &beta, c, 1, ldc); }
+template <> inline void gemm<ccscmplx>(trans_t transa, trans_t transb, dim_t m, dim_t n, dim_t k, ccscmplx alpha, ccscmplx *a, inc_t lda, ccscmplx *b, inc_t ldb, ccscmplx beta, ccscmplx *c, inc_t ldc)
+{ bli_cgemm(transa, transb, m, n, k, (scomplex *)&alpha, (scomplex *)a, 1, lda, (scomplex *)b, 1, ldb, (scomplex *)&beta, (scomplex *)c, 1, ldc); }
+template <> inline void gemm<ccdcmplx>(trans_t transa, trans_t transb, dim_t m, dim_t n, dim_t k, ccdcmplx alpha, ccdcmplx *a, inc_t lda, ccdcmplx *b, inc_t ldb, ccdcmplx beta, ccdcmplx *c, inc_t ldc)
+{ bli_zgemm(transa, transb, m, n, k, (dcomplex *)&alpha, (dcomplex *)a, 1, lda, (dcomplex *)b, 1, ldb, (dcomplex *)&beta, (dcomplex *)c, 1, ldc); }
 
 
 // ger
-extern "C" {
-void sger_(unsigned *m, unsigned *n, float *alpha, 
-           float *x, unsigned *incx, float *y, unsigned *incy, float *a, unsigned *lda);
-void dger_(unsigned *m, unsigned *n, double *alpha, 
-           double *x, unsigned *incx, double *y, unsigned *incy, double *a, unsigned *lda);
-void cgeru_(unsigned *m, unsigned *n, void *alpha, 
-            void *x, unsigned *incx, void *y, unsigned *incy, void *a, unsigned *lda);
-void zgeru_(unsigned *m, unsigned *n, void *alpha, 
-            void *x, unsigned *incx, void *y, unsigned *incy, void *a, unsigned *lda);
-}
-inline void ger(unsigned m, unsigned n, float alpha, 
-                float *x, unsigned incx, float *y, unsigned incy, float *a, unsigned lda)
-{ sger_(&m, &n, &alpha, x, &incx, y, &incy, a, &lda); }
-inline void ger(unsigned m, unsigned n, double alpha, 
-                double *x, unsigned incx, double *y, unsigned incy, double *a, unsigned lda)
-{ dger_(&m, &n, &alpha, x, &incx, y, &incy, a, &lda); }
-inline void ger(unsigned m, unsigned n, scomplex alpha, 
-                scomplex *x, unsigned incx, scomplex *y, unsigned incy, scomplex *a, unsigned lda)
-{ cgeru_(&m, &n, &alpha, x, &incx, y, &incy, a, &lda); }
-inline void ger(unsigned m, unsigned n, dcomplex alpha, 
-                dcomplex *x, unsigned incx, dcomplex *y, unsigned incy, dcomplex *a, unsigned lda)
-{ zgeru_(&m, &n, &alpha, x, &incx, y, &incy, a, &lda); }
+template <typename T>
+inline void ger(dim_t m, dim_t n,
+                T alpha,
+                T *x, inc_t incx,
+                T *y, inc_t incy,
+                T *a, inc_t lda);
+template <> inline void ger<float>(dim_t m, dim_t n, float alpha, float *x, inc_t incx, float *y, inc_t incy, float *a, inc_t lda)
+{ bli_sger(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, m, n, &alpha, x, incx, y, incy, a, 1, lda); }
+template <> inline void ger<double>(dim_t m, dim_t n, double alpha, double *x, inc_t incx, double *y, inc_t incy, double *a, inc_t lda)
+{ bli_dger(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, m, n, &alpha, x, incx, y, incy, a, 1, lda); }
+template <> inline void ger<ccscmplx>(dim_t m, dim_t n, ccscmplx alpha, ccscmplx *x, inc_t incx, ccscmplx *y, inc_t incy, ccscmplx *a, inc_t lda)
+{ bli_cger(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, m, n, (scomplex *)&alpha, (scomplex *)x, incx, (scomplex *)y, incy, (scomplex *)a, 1, lda); }
+template <> inline void ger<ccdcmplx>(dim_t m, dim_t n, ccdcmplx alpha, ccdcmplx *x, inc_t incx, ccdcmplx *y, inc_t incy, ccdcmplx *a, inc_t lda)
+{ bli_zger(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, m, n, (dcomplex *)&alpha, (dcomplex *)x, incx, (dcomplex *)y, incy, (dcomplex *)a, 1, lda); }
 
 
 // gemv
-extern "C" {
-void sgemv_(char *trans, unsigned *m, unsigned *n, float *alpha, float *a, unsigned *lda, 
-            float *x, unsigned *incx, float *beta, float *y, unsigned *incy);
-void dgemv_(char *trans, unsigned *m, unsigned *n, double *alpha, double *a, unsigned *lda, 
-            double *x, unsigned *incx, double *beta, double *y, unsigned *incy);
-void cgemv_(char *trans, unsigned *m, unsigned *n, void *alpha, void *a, unsigned *lda, 
-            void *x, unsigned *incx, void *beta, void *y, unsigned *incy);
-void zgemv_(char *trans, unsigned *m, unsigned *n, void *alpha, void *a, unsigned *lda, 
-            void *x, unsigned *incx, void *beta, void *y, unsigned *incy);
-}
-inline void gemv(char trans, unsigned m, unsigned n, float alpha, float *a, unsigned lda,
-                 float *x, unsigned incx, float beta, float *y, unsigned incy)
-{ sgemv_(&trans, &m, &n, &alpha, a, &lda, x, &incx, &beta, y, &incy); }
-inline void gemv(char trans, unsigned m, unsigned n, double alpha, double *a, unsigned lda,
-                 double *x, unsigned incx, double beta, double *y, unsigned incy)
-{ dgemv_(&trans, &m, &n, &alpha, a, &lda, x, &incx, &beta, y, &incy); }
-inline void gemv(char trans, unsigned m, unsigned n, scomplex alpha, scomplex *a, unsigned lda,
-                 scomplex *x, unsigned incx, scomplex beta, scomplex *y, unsigned incy)
-{ cgemv_(&trans, &m, &n, &alpha, a, &lda, x, &incx, &beta, y, &incy); }
-inline void gemv(char trans, unsigned m, unsigned n, dcomplex alpha, dcomplex *a, unsigned lda,
-                 dcomplex *x, unsigned incx, dcomplex beta, dcomplex *y, unsigned incy)
-{ zgemv_(&trans, &m, &n, &alpha, a, &lda, x, &incx, &beta, y, &incy); }
+template <typename T>
+inline void gemv(trans_t trans,
+                 dim_t m, dim_t n,
+                 T alpha,
+                 T *a, inc_t lda,
+                 T *x, inc_t incx,
+                 T beta,
+                 T *y, inc_t incy);
+template <> inline void gemv<float>(trans_t trans, dim_t m, dim_t n, float alpha, float *a, inc_t lda, float *x, inc_t incx, float beta, float *y, inc_t incy)
+{ bli_sgemv(trans, BLIS_NO_CONJUGATE, m, n, &alpha, a, 1, lda, x, incx, &beta, y, incy); }
+template <> inline void gemv<double>(trans_t trans, dim_t m, dim_t n, double alpha, double *a, inc_t lda, double *x, inc_t incx, double beta, double *y, inc_t incy)
+{ bli_dgemv(trans, BLIS_NO_CONJUGATE, m, n, &alpha, a, 1, lda, x, incx, &beta, y, incy); }
+template <> inline void gemv<ccscmplx>(trans_t trans, dim_t m, dim_t n, ccscmplx alpha, ccscmplx *a, inc_t lda, ccscmplx *x, inc_t incx, ccscmplx beta, ccscmplx *y, inc_t incy)
+{ bli_cgemv(trans, BLIS_NO_CONJUGATE, m, n, (scomplex *)&alpha, (scomplex *)a, 1, lda, (scomplex *)x, incx, (scomplex *)&beta, (scomplex *)y, incy); }
+template <> inline void gemv<ccdcmplx>(trans_t trans, dim_t m, dim_t n, ccdcmplx alpha, ccdcmplx *a, inc_t lda, ccdcmplx *x, inc_t incx, ccdcmplx beta, ccdcmplx *y, inc_t incy)
+{ bli_zgemv(trans, BLIS_NO_CONJUGATE, m, n, (dcomplex *)&alpha, (dcomplex *)a, 1, lda, (dcomplex *)x, incx, (dcomplex *)&beta, (dcomplex *)y, incy); }
 
 
 // swap
-extern "C" {
-void sswap_(unsigned *n, float *sx, unsigned *incx, float *sy, unsigned *incy);
-void dswap_(unsigned *n, double *sx, unsigned *incx, double *sy, unsigned *incy);
-void cswap_(unsigned *n, void *sx, unsigned *incx, void *sy, unsigned *incy);
-void zswap_(unsigned *n, void *sx, unsigned *incx, void *sy, unsigned *incy);
-}
-inline void swap(unsigned n, float *x, unsigned incx, float *y, unsigned incy)
-{ sswap_(&n, x, &incx, y, &incy); }
-inline void swap(unsigned n, double *x, unsigned incx, double *y, unsigned incy)
-{ dswap_(&n, x, &incx, y, &incy); }
-inline void swap(unsigned n, scomplex *x, unsigned incx, scomplex *y, unsigned incy)
-{ cswap_(&n, x, &incx, y, &incy); }
-inline void swap(unsigned n, dcomplex *x, unsigned incx, dcomplex *y, unsigned incy)
-{ zswap_(&n, x, &incx, y, &incy); }
+template <typename T>
+inline void swap(dim_t n,
+                 T *x, inc_t incx,
+                 T *y, inc_t incy);
+template <> inline void swap<float>(dim_t n, float *x, inc_t incx, float *y, inc_t incy)
+{ bli_sswapv(n, x, incx, y, incy); }
+template <> inline void swap<double>(dim_t n, double *x, inc_t incx, double *y, inc_t incy)
+{ bli_dswapv(n, x, incx, y, incy); }
+template <> inline void swap<ccscmplx>(dim_t n, ccscmplx *x, inc_t incx, ccscmplx *y, inc_t incy)
+{ bli_cswapv(n, (scomplex *)x, incx, (scomplex *)y, incy); }
+template <> inline void swap<ccdcmplx>(dim_t n, ccdcmplx *x, inc_t incx, ccdcmplx *y, inc_t incy)
+{ bli_zswapv(n, (dcomplex *)x, incx, (dcomplex *)y, incy); }
 
 // axpy
-extern "C" {
-void saxpy_(unsigned *n, float *alpha, float *sx, unsigned *incx, float *sy, unsigned *incy);
-void daxpy_(unsigned *n, double *alpha, double *sx, unsigned *incx, double *sy, unsigned *incy);
-void caxpy_(unsigned *n, void *alpha, void *sx, unsigned *incx, void *sy, unsigned *incy);
-void zaxpy_(unsigned *n, void *alpha, void *sx, unsigned *incx, void *sy, unsigned *incy);
-}
-inline void axpy(unsigned n, float alpha, float *x, unsigned incx, float *y, unsigned incy)
-{ saxpy_(&n, &alpha, x, &incx, y, &incy); }
-inline void axpy(unsigned n, double alpha, double *x, unsigned incx, double *y, unsigned incy)
-{ daxpy_(&n, &alpha, x, &incx, y, &incy); }
-inline void axpy(unsigned n, scomplex alpha, scomplex *x, unsigned incx, scomplex *y, unsigned incy)
-{ caxpy_(&n, &alpha, x, &incx, y, &incy); }
-inline void axpy(unsigned n, dcomplex alpha, dcomplex *x, unsigned incx, dcomplex *y, unsigned incy)
-{ zaxpy_(&n, &alpha, x, &incx, y, &incy); }
+template <typename T>
+inline void axpy(dim_t n,
+                 T alpha,
+                 T *x, inc_t incx,
+                 T *y, inc_t incy);
+template <> inline void axpy<float>(dim_t n, float alpha, float *x, inc_t incx, float *y, inc_t incy)
+{ bli_saxpyv(BLIS_NO_CONJUGATE, n, &alpha, x, incx, y, incy); }
+template <> inline void axpy<double>(dim_t n, double alpha, double *x, inc_t incx, double *y, inc_t incy)
+{ bli_daxpyv(BLIS_NO_CONJUGATE, n, &alpha, x, incx, y, incy); }
+template <> inline void axpy<ccscmplx>(dim_t n, ccscmplx alpha, ccscmplx *x, inc_t incx, ccscmplx *y, inc_t incy)
+{ bli_caxpyv(BLIS_NO_CONJUGATE, n, (scomplex *)&alpha, (scomplex *)x, incx, (scomplex *)y, incy); }
+template <> inline void axpy<ccdcmplx>(dim_t n, ccdcmplx alpha, ccdcmplx *x, inc_t incx, ccdcmplx *y, inc_t incy)
+{ bli_zaxpyv(BLIS_NO_CONJUGATE, n, (dcomplex *)&alpha, (dcomplex *)x, incx, (dcomplex *)y, incy); }
 
 // dot
-extern "C" {
-float sdot_(unsigned *n, float *sx, unsigned *incx, float *sy, unsigned *incy);
-double ddot_(unsigned *n, double *sx, unsigned *incx, double *sy, unsigned *incy);
-cscomplex cdotc_(unsigned *n, void *sx, unsigned *incx, void *sy, unsigned *incy);
-cdcomplex zdotc_(unsigned *n, void *sx, unsigned *incx, void *sy, unsigned *incy);
-}
-inline float dot(unsigned n, float *sx, unsigned incx, float *sy, unsigned incy)
-{ return sdot_(&n, sx, &incx, sy, &incy); }
-inline double dot(unsigned n, double *sx, unsigned incx, double *sy, unsigned incy)
-{ return ddot_(&n, sx, &incx, sy, &incy); }
-inline scomplex dot(unsigned n, scomplex *sx, unsigned incx, scomplex *sy, unsigned incy)
-{ cscomplex inner = cdotc_(&n, sx, &incx, sy, &incy);
-  return scomplex(inner.real, inner.imag); }
-inline dcomplex dot(unsigned n, dcomplex *sx, unsigned incx, dcomplex *sy, unsigned incy)
-{ cdcomplex inner = zdotc_(&n, sx, &incx, sy, &incy);
-  return dcomplex(inner.real, inner.imag); }
+template <typename T>
+inline T dot(dim_t n,
+             T *sx, inc_t incx,
+             T *sy, inc_t incy);
+template <> inline float dot<float>(dim_t n, float *sx, inc_t incx, float *sy, inc_t incy)
+{ float rho; bli_sdotv(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, n, sx, incx, sy, incy, &rho); return rho; }
+template <> inline double dot<double>(dim_t n, double *sx, inc_t incx, double *sy, inc_t incy)
+{ double rho; bli_ddotv(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, n, sx, incx, sy, incy, &rho); return rho; }
+template <> inline ccscmplx dot<ccscmplx>(dim_t n, ccscmplx *sx, inc_t incx, ccscmplx *sy, inc_t incy)
+{ ccscmplx rho; bli_cdotv(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, n, (scomplex *)sx, incx, (scomplex *)sy, incy, (scomplex *)&rho); return rho; }
+template <> inline ccdcmplx dot<ccdcmplx>(dim_t n, ccdcmplx *sx, inc_t incx, ccdcmplx *sy, inc_t incy)
+{ ccdcmplx rho; bli_zdotv(BLIS_NO_CONJUGATE, BLIS_NO_CONJUGATE, n, (dcomplex *)sx, incx, (dcomplex *)sy, incy, (dcomplex *)&rho); return rho; }
+
+
+// [extension] skr2k
+template <typename T>
+inline void skr2k(uplo_t uploc,
+                  trans_t transab,
+                  dim_t m, dim_t k,
+                  T alpha,
+                  T *a, inc_t lda,
+                  T *b, inc_t ldb,
+                  T beta,
+                  T *c, inc_t ldc);
+template <> inline void skr2k<float>(uplo_t uploc, trans_t transab, dim_t m, dim_t k, float alpha, float *a, inc_t lda, float *b, inc_t ldb, float beta, float *c, inc_t ldc)
+{ bli_sskr2k(uploc, transab, transab, m, k, &alpha, a, 1, lda, b, 1, ldb, &beta, c, 1, ldc); }
+template <> inline void skr2k<double>(uplo_t uploc, trans_t transab, dim_t m, dim_t k, double alpha, double *a, inc_t lda, double *b, inc_t ldb, double beta, double *c, inc_t ldc)
+{ bli_dskr2k(uploc, transab, transab, m, k, &alpha, a, 1, lda, b, 1, ldb, &beta, c, 1, ldc); }
+template <> inline void skr2k<ccscmplx>(uplo_t uploc, trans_t transab, dim_t m, dim_t k, ccscmplx alpha, ccscmplx *a, inc_t lda, ccscmplx *b, inc_t ldb, ccscmplx beta, ccscmplx *c, inc_t ldc)
+{ bli_cskr2k(uploc, transab, transab, m, k, (scomplex *)&alpha, (scomplex *)a, 1, lda, (scomplex *)b, 1, ldb, (scomplex *)&beta, (scomplex *)c, 1, ldc); }
+template <> inline void skr2k<ccdcmplx>(uplo_t uploc, trans_t transab, dim_t m, dim_t k, ccdcmplx alpha, ccdcmplx *a, inc_t lda, ccdcmplx *b, inc_t ldb, ccdcmplx beta, ccdcmplx *c, inc_t ldc)
+{ bli_zskr2k(uploc, transab, transab, m, k, (dcomplex *)&alpha, (dcomplex *)a, 1, lda, (dcomplex *)b, 1, ldb, (dcomplex *)&beta, (dcomplex *)c, 1, ldc); }
